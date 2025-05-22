3b41727b352993e96351d68300404c2d52552c5d
Pedro gabriel <pedrooliveiraquintal@gmail.com>
2025-05-22 14:56:29 -0300

    Adicionar foto de perfil

--- MauiApp1/Views/PaginaPerfil.xaml.cs
+++ MauiApp1/Views/PaginaPerfil.xaml.cs
@@ -6,4 +6,25 @@ public partial class PaginaPerfil : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        try
+        {
+            var resultado = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Escolha uma imagem"
+            });
+
+            if (resultado != null)
+            {
+                var stream = await resultado.OpenReadAsync();
+                minhaImagem.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+        catch (Exception ex)
+        {
+            await Application.Current.MainPage.DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
 }
